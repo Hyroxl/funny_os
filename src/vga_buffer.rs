3cec0dd4a2181d659c4879cbc5766d46fa2f50f2
hyroxl <tylerpaul926@icloud.com>
2023-03-09 20:57:07 -0500

    implement formatting macros

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -96,6 +96,7 @@ impl Writer {
 
 
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
@@ -103,7 +104,16 @@ pub fn print_something() {
     };
 
     writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    writer.write_string("ello! ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+}
+
+use core::fmt;
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
 }
 
