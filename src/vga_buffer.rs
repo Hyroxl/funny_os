6b16cb2b8a28978ca6813ff18e7563ef285f995d
hyroxl <tylerpaul926@icloud.com>
2023-03-09 21:04:41 -0500

    added new line capanility

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -77,8 +77,29 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {/* TODO */}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+    }
+    self.clear_row(BUFFER_HEIGHT - 1);
+    self.column_position = 0;
 }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+
 
 impl Writer {
     pub fn write_string(&mut self, s: &str) {
