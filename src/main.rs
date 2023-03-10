dbead2925c8b0f6f480f613fa52ab6639af34d73
hyroxl <tylerpaul926@icloud.com>
2023-03-09 21:29:29 -0500

    finished page three of tutorial

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,14 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+pub extern "C" fn _start() {
+    println!("Hello, World{}", "!");
 
     loop {}
 }
