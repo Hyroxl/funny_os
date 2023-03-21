f6ac319c290f4ffc29bb170bb18467ab4d227df8
hyroxl <tylerpaul926@icloud.com>
2023-03-21 16:34:32 -0400

    should_panic test added

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,27 +7,19 @@
 use core::panic::PanicInfo;
 use blog_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
 }
 
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
